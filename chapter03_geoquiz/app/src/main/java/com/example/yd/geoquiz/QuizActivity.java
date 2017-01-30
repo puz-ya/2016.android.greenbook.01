a28fa07fc102ab36f79ae94b3337b54e716196c2
puz_ya <puzino.y.a@gmail.com>
2017-01-30 22:26:46 +0300

    Checking Activity Lifecycle by logCat.

--- chapter03_geoquiz/app/src/main/java/com/example/yd/geoquiz/QuizActivity.java
+++ chapter03_geoquiz/app/src/main/java/com/example/yd/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.yd.geoquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mNextButton;
     private Button mPrevButton;
 
+    private static final String TAG = "QuizAct";
+
     //неправильно, но пока так
     private Question[] mQuestions = new Question[]{
             new Question(R.string.question_oceans,true),
@@ -33,6 +36,9 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set log
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -91,6 +97,36 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     //set new question

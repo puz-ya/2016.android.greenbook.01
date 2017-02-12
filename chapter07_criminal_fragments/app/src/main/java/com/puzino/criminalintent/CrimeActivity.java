ccde040c798b78c4affcea80f75e135e0596cf68
puz_ya <puzino.y.a@gmail.com>
2017-02-12 23:43:23 +0300

    Added Crime model; Added fragment insides; Added fragment transaction onCreate activity.

--- chapter07_criminal_fragments/app/src/main/java/com/puzino/criminalintent/CrimeActivity.java
+++ chapter07_criminal_fragments/app/src/main/java/com/puzino/criminalintent/CrimeActivity.java
@@ -1,7 +1,14 @@
 package com.puzino.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created on 10.02.2017
+ * @author Puzino Yury.
+ */
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,5 +16,17 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //support Fragments! v4 only
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
